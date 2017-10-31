b4a31870a2fecd6dad4ba1e9058ddc2bb2553817
Marcin Tymoszewski <tucinator@gmail.com>
2017-10-31 19:31:41 +0100

    taking BaW photo

--- Camera.py
+++ Camera.py
@@ -2,17 +2,24 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 1920);
+cap.set(4, 1080);
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame', frame)
+    k = cv2.waitKey(1) & 0xFF
+
+    if k == ord('t'):
+        print("Taking image...")
+        photo = "pictures\photo.png"
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(photo, gray)
+        print("Done!")
+    elif k == ord('q'):
         break
 
 # When everything done, release the capture
