98a2c6bf72d98c61868914afb9232577c8c15f40
Marcin Tymoszewski <tucinator@gmail.com>
2017-11-07 19:25:11 +0100

    working camera and processing

--- Camera.py
+++ Camera.py
@@ -1,27 +1,30 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1920);
-cap.set(4, 1080);
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+def take_photo():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+    photo = ""
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(1) & 0xFF
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(1) & 0xFF
 
-    if k == ord('t'):
-        print("Taking image...")
-        photo = "pictures\photo.png"
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(photo, gray)
-        print("Done!")
-    elif k == ord('q'):
-        break
+        if k == ord('t'):
+            print("Taking image...")
+            photo = "pictures\photo.png"
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(photo, frame)
+            print("Done!")
+        elif k == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+    return photo
